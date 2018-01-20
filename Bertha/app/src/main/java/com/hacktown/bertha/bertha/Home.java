98005894404b957289da3c985aae9f8017b25140
Salim Eid <salim.eid@polymtl.ca>
2018-01-20 14:51:21 -0500

    Linked buttons from home to activities
    Created events for buttons from Home activity to appropriate activities.

--- Bertha/app/src/main/java/com/hacktown/bertha/bertha/Home.java
+++ Bertha/app/src/main/java/com/hacktown/bertha/bertha/Home.java
@@ -1,7 +1,10 @@
 package com.hacktown.bertha.bertha;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import com.hacktown.bertha.myapplication.R;
 
 public class Home extends AppCompatActivity {
@@ -10,5 +13,23 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        final Button getHelpBtn = findViewById(R.id.gethelp);
+
+        final Button volunteerBtn = findViewById(R.id.volunteer);
+
+        getHelpBtn.setOnClickListener(new View.OnClickListener(){
+           public void onClick(View v){
+               Intent openHelpChat = new Intent(Home.this, Chat.class);
+               startActivity(openHelpChat);
+           }
+        });
+
+        volunteerBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent openVolunteerMenu = new Intent(Home.this, VolunteerSelection.class);
+                startActivity(openVolunteerMenu);
+            }
+        });
     }
 }
