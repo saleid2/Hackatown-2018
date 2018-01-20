41862e742828e69c3d20f7028844e28b934ac472
Salim Eid <salim.eid@polymtl.ca>
2018-01-20 14:43:09 -0500

    Created activites
    Created activity for Seleccting the type of user.
    Created activity for Volunteer menu (tabbed activity).
    Created activity for Chat interface.

---
+++ Bertha/app/src/main/java/com/hacktown/bertha/bertha/Chat.java
@@ -0,0 +1,14 @@
+package com.hacktown.bertha.bertha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.hacktown.bertha.myapplication.R;
+
+public class Chat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
